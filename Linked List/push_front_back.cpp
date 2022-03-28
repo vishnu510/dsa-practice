b60aaec0f9c9c1fba536600ab1e68f4ae9755a45
vishnu510 <vpal6044@gmail.com>
2022-03-29 00:05:41 +0530

    Linked List Push

--- Linked List/push_front_back.cpp
+++ Linked List/push_front_back.cpp
@@ -1,6 +1,52 @@
 #include<iostream>
-#include"list.h"
 using namespace std;
+
+class List;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int d) : data(d),next(NULL){}
+    int getData(){
+        return data;
+    } 
+
+    friend class List;
+};
+class List{
+    Node* head;
+    Node* tail;
+    public:
+    List() : head(NULL),tail(NULL){}
+
+    // pushing data in linked list at front
+    void push_front(int data){
+        if(head==NULL){
+            Node* n = new Node(data);
+            head  = tail  = n;
+        }
+        else{
+            Node* n =new Node(data);
+            n->next = head;
+            head = n;
+        }
+    }
+
+    // pushing data in linked list at back
+
+    void push_back(int data){
+       if(head==NULL){
+           Node* n = new Node(data);
+           n->next = head;
+           head  = tail = n;
+       }
+       else{
+           Node* n = new Node(data);
+           tail->next = n;
+           tail = n;
+       }
+    }
+};
 
 
 int main(){
