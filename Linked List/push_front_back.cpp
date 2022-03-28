dfadec27b2ee851e050089cd8f2748a1adfee11b
vishnu510 <vpal6044@gmail.com>
2022-03-28 23:56:17 +0530

    Linked List Push

---
+++ Linked List/push_front_back.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"list.h"
+using namespace std;
+
+
+int main(){
+    List l;
+    l.push_front(1);
+    l.push_front(0);
+    l.push_back(2);
+
+    Node* head = l.begin();
+    while(head!=NULL){
+        cout<<head->getData()<<"->";
+        head = head->next;
+    }
+    return 0;
+}
